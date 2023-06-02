6c5b099895c7874baa738944d3afecbccf73fade
studeyang <studeyang@gmail.com>
2023-06-02 18:29:56 +0800

    feat: 支持新版本springBoot&支持Java17

--- start-site/src/main/java/io/spring/start/site/support/StartInitializrMetadataUpdateStrategy.java
+++ start-site/src/main/java/io/spring/start/site/support/StartInitializrMetadataUpdateStrategy.java
@@ -18,24 +18,48 @@ package io.spring.start.site.support;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.spring.initializr.generator.version.Version;
+import io.spring.initializr.generator.version.VersionParser;
 import io.spring.initializr.metadata.DefaultMetadataElement;
+import io.spring.initializr.metadata.InitializrMetadata;
 import io.spring.initializr.web.support.InitializrMetadataUpdateStrategy;
 import io.spring.initializr.web.support.SpringIoInitializrMetadataUpdateStrategy;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * An {@link InitializrMetadataUpdateStrategy} that performs additional filtering of
  * versions available on spring.io.
+ * <p>修改成 current bootVersion + updated bootVersion</p>
  *
  * @author Stephane Nicoll
+ * @author <a href="https://github.com/studeyang">studeyang</a>
  */
 public class StartInitializrMetadataUpdateStrategy extends SpringIoInitializrMetadataUpdateStrategy {
 
+    private static final Comparator<DefaultMetadataElement> VERSION_METADATA_ELEMENT_COMPARATOR = new VersionMetadataElementComparator();
+
     public StartInitializrMetadataUpdateStrategy(RestTemplate restTemplate, ObjectMapper objectMapper) {
         super(restTemplate, objectMapper);
+    }
+
+    @Override
+    public InitializrMetadata update(InitializrMetadata current) {
+        String url = current.getConfiguration().getEnv().getSpringBootMetadataUrl();
+        List<DefaultMetadataElement> bootVersions = fetchSpringBootVersions(url);
+
+        if (bootVersions != null && !bootVersions.isEmpty()) {
+            bootVersions.addAll(current.getBootVersions().getContent());
+            if (bootVersions.stream().noneMatch(DefaultMetadataElement::isDefault)) {
+                // No default specified
+                bootVersions.get(0).setDefault(true);
+            }
+            bootVersions.sort(VERSION_METADATA_ELEMENT_COMPARATOR);
+            current.updateSpringBootVersions(bootVersions);
+        }
+        return current;
     }
 
     @Override
@@ -48,6 +72,19 @@ public class StartInitializrMetadataUpdateStrategy extends SpringIoInitializrMet
     private boolean isCompatibleVersion(DefaultMetadataElement versionMetadata) {
         Version version = Version.parse(versionMetadata.getId());
         return (version.getMajor() == 2 && version.getMinor() > 4) || (version.getMajor() >= 3);
+    }
+
+    private static class VersionMetadataElementComparator implements Comparator<DefaultMetadataElement> {
+
+        private static final VersionParser versionParser = VersionParser.DEFAULT;
+
+        @Override
+        public int compare(DefaultMetadataElement o1, DefaultMetadataElement o2) {
+            Version o1Version = versionParser.parse(o1.getId());
+            Version o2Version = versionParser.parse(o2.getId());
+            return o1Version.compareTo(o2Version);
+        }
+
     }
 
 }
